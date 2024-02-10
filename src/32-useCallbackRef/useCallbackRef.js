10468e2229476fd3eaf22aa32a1b7e73489c57eb
dev99 <arssw71@gmail.com>
2024-02-10 13:25:00 +0330

    adding useCallbackRef for functions as useEffect dependency

---
+++ src/32-useCallbackRef/useCallbackRef.js
@@ -0,0 +1,9 @@
+import { useRef, useLayoutEffect } from "react";
+
+export const useCallbackRef = (callback) => {
+  const callbackRef = useRef(callback);
+  useLayoutEffect(() => {
+    callbackRef.current = callback;
+  }, [callback]);
+  return callbackRef;
+};
