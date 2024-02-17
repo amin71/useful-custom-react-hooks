128a47ad6c6ac64da4a76acdcb466c28b559ec1b
dev99 <arssw71@gmail.com>
2024-02-17 13:20:34 +0330

    adding useDialogModal

---
+++ src/34-useDialogModal/useDialogModal.js
@@ -0,0 +1,24 @@
+import { useCallback, useState } from "react";
+
+export default function useDialogModal(Component) {
+  const [open, setOpen] = useState(false);
+
+  const openDialog = useCallback(() => {
+    setOpen(true);
+  }, []);
+
+  const closeDialog = useCallback(() => setOpen(false), []);
+
+  const DialogComponent = useCallback(
+    ({ ...props }) => {
+      if (!open) return null;
+
+      if (Component) {
+        return <Component open={open} onClose={closeDialog} {...props} />;
+      }
+    },
+    [open, Component]
+  );
+
+  return [DialogComponent, openDialog, closeDialog];
+}
